8720763d29084fa874e3d77084a6f8fd4b306de1
zhangzedong <983521365@qq.com>
2020-05-27 18:15:24 +0800

    设置登陆界面语言选择

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,7 @@
 import { ComponentType } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { observer, inject } from '@tarojs/mobx'
-import { View, Form, Button, Text, Image, Picker } from '@tarojs/components'
-import { AtInput, AtButton, AtDivider } from 'taro-ui'
+import {  auth, lang } from '@utils'
 
 import './login.scss'
 
@@ -12,6 +11,12 @@ type PageStateProps = {
     increment: Function,
     decrement: Function,
     incrementAsync: Function
+  },
+  loginStore: {
+    form: Object,
+    languages:Array,
+    activeLang: string,
+    changeForm: Function
   },
   loginType?: string
 }
@@ -34,6 +39,7 @@ class Login extends Component {
   config: Config = {
     navigationBarTitleText: '登陆页面'
   }
+
 
   componentWillMount () { 
     console.log('login componentWillMount')
@@ -65,59 +71,28 @@ class Login extends Component {
       url: '/pages/index/index'
     })
   }
+
+  setLang(val) {
+
+  }
   render () {
-    const { countDown, form, disType, lang, countries, country } = this.state;
-    const config = cfg.get('config');
-  
+    const {loginStore } = this.props;
+    // onClick={ _ => this.setLng(item.value) } 
     return (
       <View className='block'>
-        {!config ? null : <View style={{ textAlign:'center', fontSize: 24 }}>
-          {!config.logo ? null : <Image mode="aspectFit" style='height: 80px;background: #fff;' src={config.logo} />}
-          {!config.title ? null : <View>{ config.title }</View>}
-        </View>}
-        <AtDivider content={ disType === 'pwdLogin' ? '密码登录' : disType === 'reg' ? '注册新账号' : lng.get(['member','login']) } fontColor='#2d8cf0' lineColor='#2d8cf0' />
-  
-        { disType !== 'reg' ? null : <AtInput name='realName' title='姓名' type='text' maxLength='3' placeholder='姓名' value={form.realName} onChange={ realName => this.setValue({ realName }) } />}
-  
-        <AtInput ref={ d => this.p = d } name='phone' title={lng.get('phone')} type='text' maxLength='15' placeholder={lng.get('phone')} value={form.phone} onChange={ phone => this.setValue({ phone }) }>
-          <Picker mode='selector' rangeKey="name" range={ countries } onChange={({ detail: { value } }) => this.setState({ country: value })}>
-            <View className='picker'>
-              { countries[country].name }
-            </View>
-          </Picker>
-        </AtInput>
-  
-        {
-          disType === 'pwdLogin' ? null :
-          <AtInput name='phonecode' title={lng.get('validCode')} type='text' maxLength='6' placeholder={lng.get('validCode')} value={form.phonecode} onChange={ phonecode => this.setValue({ phonecode }) }>
-            { disType === 'pwdLogin' ? null : countDown && countDown > 0 ? `${countDown} ${lng.get('tryLater')}` : <AtButton size='small' onClick={ _ => this.onCode() }>{lng.get('validCode')}</AtButton>}
-          </AtInput>
-        }
-        {
-          disType === 'phoneLogin' ? null :
-          <AtInput name='pwd' title='密码' type='password' maxLength='16' placeholder='密码' value={form.pwd} onChange={ pwd => this.setValue({ pwd }) } />
-        }
-  
-        <Button onClick={ _ => this.onLogin() } className='at-button at-button--normal at-button--primary'>{lng.get('submit')}</Button>
-  
-        <View className='at-row margin-h' style='display:none'>
-          <View className='at-col at-col-6'>
-            <Text onClick={ _ => this.setState({ disType: disType === 'pwdLogin' ? 'phoneLogin' : 'pwdLogin' }) }>{ disType === 'pwdLogin' ? '手机号码登录' : '密码登录' }</Text>
-          </View>
-          { !cfg.allowReg ? null : <View className='at-col at-col-6' style='text-align:right;'>
-            <Text onClick={ _ => this.setState({ disType: 'reg' }) }>注册新账号</Text>
-          </View>}
-        </View>
+       
         <View className='at-row at-row__justify--around' style='margin-top:20px;text-align:center'>
-          <View className='at-col' onClick={ _ => this.setLng('zh') } style={{ borderBottom: lang==='zh'?'solid #6392E5 1px':'0' }}>
-            中文
-          </View>
-          <View className='at-col' onClick={ _ => this.setLng('en') } style={{ borderBottom: lang==='en'?'solid #6392E5 1px':'0' }}>
-            English
-          </View>
-          <View className='at-col' onClick={ _ => this.setLng('ja') } style={{ borderBottom: lang==='ja'?'solid #6392E5 1px':'0' }}>
-            日本語
-          </View>
+          {
+            loginStore.languages.map( (item ) => {
+              let cName = item.value === loginStore.activeLang ? 'at-col border-bottom' : 'at-col';
+              return (
+                <View className={cName} onClick={this.setLang(item.value)} >
+                {item.text}
+              </View>
+              )
+            })
+           
+          }
         </View>
       </View>
       )
--- src/store/login.ts
+++ src/store/login.ts
@@ -7,6 +7,13 @@ const loginStore = observable({
     phone: '',
     code: ''
   },
+  languages: [
+    { text: '中文', value: 'zh',},
+    { text: 'English', value: 'en' },
+    { text: '日本語', value: 'ja' }
+    
+  ],
+  activeLang: 'zh',
   changeForm(form) {
     this.form = form;
   }
